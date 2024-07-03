a96d29ad8879896a219e2328816720c88a1c19df
nPaBwaYT <surge.anton@gmail.com>
2024-07-03 16:04:57 +0300

    InitialCommit

---
+++ src/TrackerService/ApiControllers/GoodsApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrackerService.ApiControllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class GoodsApiController
+{
+    [HttpGet(nameof(Get))]
+    public int Get(int id)
+    {
+        return id;
+    }
+}
---
+++ src/TrackerService/Startup.cs
@@ -0,0 +1,51 @@
+﻿namespace TrackerService;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthorization();
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
